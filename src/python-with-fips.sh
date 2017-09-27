b5696eec8f70aa84f7378111bf9bb7fb91751509
Faizan Khan <faizan.khan@tenpearls.com>
2017-09-27 14:36:18 +0500

    Some Changes

--- src/python-with-fips.sh
+++ src/python-with-fips.sh
@@ -1,3 +1,5 @@
+
+
 ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
 ln -s /usr/local/ssl/lib /usr/lib/ssl
 ln -s /usr/local/ssl/include/openssl/ /usr/include/ssl
@@ -5,6 +7,11 @@ ln -s /usr/local/ssl/include/openssl/ /usr/include/ssl
 export LDFLAGS="-L/usr/local/ssl/lib/"
 export LD_LIBRARY_PATH="/usr/local/ssl/lib/"
 export CPPFLAGS="-I/usr/local/ssl/include/ -I/usr/local/ssl/include/openssl/"
+
+cd /tmp
+tar xf Python-3.6.0.tar.xz
+rm Python-3.6.0.tar.xz
+
 
 cd /tmp/Python-3.6.0
 ./configure --enable-shared --prefix=/usr/local/python3.6
